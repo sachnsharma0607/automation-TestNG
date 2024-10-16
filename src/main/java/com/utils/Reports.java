80c850db3511782ab69796d9bee815782e64a6b6
sachin <sachinorsharma@gmail.com>
2024-10-17 04:22:20 +0530

    update old method

--- src/main/java/com/utils/Reports.java
+++ src/main/java/com/utils/Reports.java
@@ -6,24 +6,29 @@ import java.io.IOException;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class Reports implements ConstantValue {
 
 	private final String filepath = FOLDER_PATH + pathSeparator + "Report_" + new DateFormate().toString() + ".html";
-	ExtentReports extentReports = null;;
-	ExtentSparkReporter extentSparkReporter = null;
-	ExtentTest eTest=null;
+	ThreadLocal<ExtentReports> extentRepor=new ThreadLocal<>();
+	ThreadLocal<ExtentSparkReporter> extentSparkReporter = 
+			new ThreadLocal<ExtentSparkReporter>();
+	ThreadLocal<ExtentTest> eTest = new ThreadLocal<ExtentTest>();
 
 	public Reports() {
 		deleteDirectory();
-		extentReports = new ExtentReports();
-		extentSparkReporter = new ExtentSparkReporter(new File(filepath));
+		extentRepor.set(new ExtentReports());
+		extentSparkReporter.set( new ExtentSparkReporter(new File(filepath)));
 	}
 
 	public void generateReport() {
-		extentReports.attachReporter(extentSparkReporter);
-		extentReports.flush();
+
+		extentRepor.get().attachReporter(extentSparkReporter.get());
+		extentRepor.get().flush();
+//		extentReports.attachReporter(extentSparkReporter);
+//		extentReports.flush();
 		try {
 			Desktop.getDesktop().browse(new File(filepath).toURI());
 		} catch (IOException e) {
@@ -31,41 +36,50 @@ public class Reports implements ConstantValue {
 			e.printStackTrace();
 		}
 	}
-	
-	private void createTest(String testCaseName)
-	{
-		 eTest=extentReports.createTest(testCaseName);
+
+	public void createTest(String testCaseName) {
+		eTest.set(extentRepor.get().createTest(testCaseName));
+				
 	}
 
 	public static void deleteDirectory() {
 		new FileUtils().deleteDirectory(new File(FOLDER_PATH));
 	}
-	
-	public void pass(String StepDescription, String expectedResult)
-	{
+
+	public void info(String StepDescription, String expectedResult) {
+		//createTest(StepDescription);
+		// eTest.pass(expectedResult);
+		eTest.get().log(Status.INFO,    "<b>" + StepDescription+" : </b>"+ expectedResult  );
+
+	}
+
+	public void pass(String StepDescription, String expectedResult) {
+		// eTest.pass(expectedResult);
+		eTest.get().log(Status.PASS, "<b>" + StepDescription+" : </b>"+ expectedResult  );
+
+
+	}
+
+	public void waring(String StepDescription, String expectedResult) {
 		createTest(StepDescription);
-		eTest.pass(expectedResult);
-		
+		// eTest.pass(expectedResult);
+		eTest.get().log(Status.WARNING,  "<b>" + StepDescription+" : </b>"+ expectedResult  );
+
 	}
-	
-	public void fail(String StepDescription, String expectedResult)
-	{
-		createTest(StepDescription);
-		eTest.fail(expectedResult);
-		
+
+	public void fail(String StepDescription, String expectedResult) {
+		//createTest(StepDescription);
+		// eTest.fail(expectedResult);
+		eTest.get().log(Status.FAIL, "<b>" + StepDescription+" : </b>"+ expectedResult  );
+
+
 	}
-	
-	public void skip(String StepDescription, String expectedResult)
-	{
-		createTest(StepDescription);
-		eTest.skip(expectedResult);
-		
+
+	public void skip(String StepDescription, String expectedResult) {
+		//createTest(StepDescription);
+		// eTest.skip(expectedResult);
+		eTest.get().log(Status.SKIP,  "<b>" + StepDescription+" : </b>"+ expectedResult  );
+
 	}
-	
-	enum status{
-		
-		PASS,FAIL, SKIP;
-		
-		
-	}
+
 }
