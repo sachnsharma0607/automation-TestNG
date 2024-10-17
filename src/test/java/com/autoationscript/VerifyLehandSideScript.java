7aafe8aeee6cb2f6dc3aaa5c5859aac8ddefbac2
sachin <sachinorsharma@gmail.com>
2024-10-17 09:56:13 +0530

    update new test script VerifyUserAbleToLogin

--- src/test/java/com/autoationscript/VerifyLehandSideScript.java
+++ src/test/java/com/autoationscript/VerifyLehandSideScript.java
@@ -16,32 +16,31 @@ public class VerifyLehandSideScript extends BaseClass {
 
 		LeftHandSidePOM lefthandside = new LeftHandSidePOM(driver);
 		AdminPOM admin = new AdminPOM(driver);
-		
+
 		// Step 1-3
-		userableToLogin("Verify the use ");
-		
-		//Step 4:  //Verify Expend Mode
+		userableToLogin("Verify the user able to click left hand side button");
+
+		// Step 4: //Verify Expend Mode
 		assertTrue(lefthandside.verifyArroInExpandMode());
 		report.pass("Step 4", "click ON Expand Mode");
 
 		assertTrue(lefthandside.clickOnAdmin());
 		report.pass("Step 5", "Click on Admin Button on left side");
-		
-		
+
 		assertTrue(lefthandside.verifyadmin("Admin"));
 		report.pass("Step 6", "Verify the admin button");
-		
+
 		assertTrue(admin.VerifyRecords("Found"));
 		report.pass("Step 7", "Verify the records");
-		
+
 		// assertTrue(admin.VerifyRecords(""));
 
 	}
 
 	@Test
-	public void t() {
-		PIMPOM pimpom = new PIMPOM(driver);
-		userableToLogin("Verify the use ");
+	public void VerifyUserAbleToLogin() {
+		// Step 1-3
+		userableToLogin("Verify the user Able to Login");
 
 	}
 
