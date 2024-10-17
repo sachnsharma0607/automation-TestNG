0a49b371c20564b50c874ab5226d4c0b321605d2
sachin <sachinorsharma@gmail.com>
2024-10-17 09:44:11 +0530

    add report step

--- src/test/java/com/autoationscript/VerifyLehandSideScript.java
+++ src/test/java/com/autoationscript/VerifyLehandSideScript.java
@@ -16,12 +16,24 @@ public class VerifyLehandSideScript extends BaseClass {
 
 		LeftHandSidePOM lefthandside = new LeftHandSidePOM(driver);
 		AdminPOM admin = new AdminPOM(driver);
-		userableToLogin();
+		
+		// Step 1-3
+		userableToLogin("Verify the use ");
+		
+		//Step 4:  //Verify Expend Mode
 		assertTrue(lefthandside.verifyArroInExpandMode());
+		report.pass("Step 4", "click ON Expand Mode");
 
 		assertTrue(lefthandside.clickOnAdmin());
+		report.pass("Step 5", "Click on Admin Button on left side");
+		
+		
 		assertTrue(lefthandside.verifyadmin("Admin"));
+		report.pass("Step 6", "Verify the admin button");
+		
 		assertTrue(admin.VerifyRecords("Found"));
+		report.pass("Step 7", "Verify the records");
+		
 		// assertTrue(admin.VerifyRecords(""));
 
 	}
@@ -29,7 +41,7 @@ public class VerifyLehandSideScript extends BaseClass {
 	@Test
 	public void t() {
 		PIMPOM pimpom = new PIMPOM(driver);
-		pimpom.b();
+		userableToLogin("Verify the use ");
 
 	}
 
